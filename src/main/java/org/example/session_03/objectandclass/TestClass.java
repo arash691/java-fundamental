11c8fe17633a78377c9d30ad73383eb1ef8f4357
arash.sw90@gmail.com <arash.sw90@gmail.com>
2021-02-26 12:26:45 +0330

    previous sessions added

---
+++ src/main/java/org/example/session_03/objectandclass/TestClass.java
@@ -0,0 +1,9 @@
+package org.example.session_03.objectandclass;
+
+
+// define new class : access level + class + className
+// exp : public class Person {} , public class Animal {} ,
+// public class Account {} , public class TimeUtil {}
+public class TestClass {
+
+}
